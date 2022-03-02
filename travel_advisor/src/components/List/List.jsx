9864ddb52a578420b32d152b043fc1840c40500f
r.yan <yannmm@foxmail.com>
2022-03-02 22:56:56 +0800

    wip

--- travel_advisor/src/components/List/List.jsx
+++ travel_advisor/src/components/List/List.jsx
@@ -1,7 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
+import {
+  CircularProgress,
+  Grid,
+  Typography,
+  InputLabel,
+  MenuItem,
+  FormControl,
+  Select,
+} from "@material-ui/core";
+import useStyles from "./styles";
 
 const List = () => {
-  return <h1>Listaaaa</h1>;
+  const classes = useStyles();
+  const [type, setType] = useState("restaurants");
+  return (
+    <div className={classes.container}>
+      <Typography variant="h4">
+        Restaurant, hotels & attactorions around you
+      </Typography>
+      <FormControl className={classes.formControl}>
+        <InputLabel>Type</InputLabel>
+        <Select value={type} onChange={(e) => setType(e.target.value)}>
+          <MenuItem value="restaurants">restaurants</MenuItem>
+          <MenuItem value="hotels">hotels</MenuItem>
+          <MenuItem value="attractions">attractions</MenuItem>
+        </Select>
+      </FormControl>
+    </div>
+  );
 };
 
 export default List;
--- travel_advisor/src/components/Map/Map.jsx
+++ travel_advisor/src/components/Map/Map.jsx
@@ -1,7 +1,31 @@
 import React from "react";
+import GoogleMapReact from "google-map-react";
+import { Paper, Typography, useMediaQuery } from "@material-ui/core";
+import LocationOnOutlinedIcon from "@material-ui/icons/LocationOnOutlined";
+import Rating from "@material-ui/lab";
+
+import useStyles from "./styles";
 
 const Map = () => {
-  return <h1>Map</h1>;
+  const classes = useStyles();
+  const isMobile = useMediaQuery("(min-width:600px)");
+  const coordinates = { lat: 39.90960456049752, lng: 116.3972282409668 };
+  return (
+    <h1>
+      <div className={classes.mapContainer}>
+        <GoogleMapReact
+          bootstrapURLKeys={{ key: "AIzaSyCvLtmnlBU_jVYSdpz4KNlGXfsoM5xmH4g" }}
+          defaultCenter={coordinates}
+          center={coordinates}
+          defaultZoom={14}
+          margin={[50, 50, 50, 50]}
+          // options={""}
+          // onChange={""}
+          // onChildClick={""}
+        ></GoogleMapReact>
+      </div>
+    </h1>
+  );
 };
 
 export default Map;
